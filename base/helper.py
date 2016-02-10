0ea76b2328537b7c03975496c4fae45287ecbc52
Sanjay Joshi <joshisa@us.ibm.com>
2016-02-10 11:36:14 -0500

    :sunrise:

---
+++ base/helper.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Provides Helper Functions while working with Bluemix Spark Notebooks.
+
+This module provides helper functions based on IBM jStart experiences.
+Enjoy!
+"""
+
+
+def smallthings():
+    print "Great things are done by a series of small things brought together."
