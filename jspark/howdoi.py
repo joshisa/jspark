dced291627b8243f7666e31a9ec887b87524687a
Sanjay Joshi <joshisa@us.ibm.com>
2016-02-11 23:03:36 -0500

    Added howdoi for codefolding extension

--- jspark/howdoi.py
+++ jspark/howdoi.py
@@ -29,3 +29,32 @@ def persist_my_rdd():
 def cleanup_an_rdd_with_errors():
     define_filename_swift_url()
     print "my_rdd = sc.textFile(filename).flatMap(lambda line: cleanup(line)).persist(pyspark.storagelevel.StorageLevel.MEMORY_AND_DISK)"
+
+
+def install_codefolding_extension():
+    print "This extension adds codefolding functionality from CodeMirror to a codecell."
+    print "After clicking on the gutter or typing Alt+F, the code gets folded."
+    print "This is useful in reducing visual noise of your notebook"
+    print ""
+    print "No. of Cells to execute: 3"
+    print "--------------------------"
+    print ""
+    print "Cell 1 - Paste text below"
+    print "# Install Notify Jupyter nbextension"
+    print "import IPython.html.nbextensions as nb"
+    print "ext= 'https://github.com/ipython-contrib/IPython-notebook-extensions/archive/3.x.zip'"
+    print "nb.install_nbextension(ext, user=True)"
+    print ""
+    print "Cell 2 - Paste text below"
+    print "%%javascript"
+    print "IPython.load_extensions('IPython-notebook-extensions-3.x/usability/codefolding/main');"
+    print ""
+    print "Cell 3 - Paste text below"
+    print "# Activate codefolding extension"
+    print "from IPython.html.services.config import ConfigManager"
+    print "ip = get_ipython()"
+    print "cm = ConfigManager(parent=ip, profile_dir=ip.profile_dir.location)"
+    print "cm.update('notebook', {'load_extensions': {'IPython-notebook-extensions-3.x/usability/codefolding/main': True}})"
+    print ""
+    print "Test:  Both Cell 1 and Cell 3 should now show a clickable triangle next to the cell's first line which is a comment"
+    print "See https://github.com/ipython-contrib/IPython-notebook-extensions/wiki/Codefolding for more details."
